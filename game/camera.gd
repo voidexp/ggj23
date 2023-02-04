330e83d0697feedd5911af1eb56f7716d31058f0
Ivan Nikolaev <voidexp@gmail.com>
2023-02-04 13:38:28 +0200

    Camera and blocks tweaks

---
+++ game/camera.gd
@@ -0,0 +1,11 @@
+extends Camera
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	look_at(Vector3.ZERO, Vector3.FORWARD)
