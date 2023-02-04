21467dc691cc1413c8d2e9ea70fb1ef375cd2e4d
Dmitriy Zaitsev <d_zaitsev@wargaming.net>
2023-02-04 20:30:01 +0200

    lol svet podlil

---
+++ game/grid_block.gd
@@ -0,0 +1,23 @@
+extends StaticBody
+
+class_name GridBlock
+
+
+var row_id
+var col_id
+
+signal destroyed
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	name = '_'.join([name, str(row_id), str(col_id)])
+
+func queue_free():
+	
+	emit_signal("destroyed", row_id, col_id)
+	.queue_free()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
