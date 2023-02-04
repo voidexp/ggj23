16757c496ee2001af89ef6a1c48d73c48d2c589a
Artur Shibaylo <shibailoav@gmail.com>
2023-02-04 03:35:44 +0200

    Added gold

--- game/level_map.gd
+++ game/level_map.gd
@@ -7,16 +7,19 @@ extends Spatial
 
 export(PackedScene) var soil_brick
 export(PackedScene) var rock_brick
+export(PackedScene) var gold_brick
 export var size_x = 21
 export var size_z = 21
 
 var GROUND_THICKNESS = 0.1
 
+var tile_map = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	__init_ground()
 	__init_tiles()
-
+	
 func __init_tiles():
 	assert(size_x % 2 == 1)
 	var start_x = -size_x / 2
@@ -26,16 +29,23 @@ func __init_tiles():
 	for i in range(size_x):
 		for j in range(size_z):
 			var position = Vector3(start_x + i, y_pos, start_z + j)
-			var brick_class = soil_brick if (i % 2 == 1) or (j % 2 == 1) else rock_brick
-			var new_brick = brick_class.instance()
+			var new_brick = __get_block_by_position(i, j).instance()
 
 			add_child(new_brick)
 			new_brick.translate(position)
 
+func __get_block_by_position(i, j):
+	if i == ceil(size_x/2) and j == ceil(size_z/2):
+		return gold_brick
+	if (i % 2 == 1) or (j % 2 == 1):
+		return soil_brick
+	return rock_brick
+	
+	
 func __init_ground():
 	var ground_size = Vector3(size_x/2, GROUND_THICKNESS/2, size_z/2)
 	$Ground/CollisionShape.shape.extents = ground_size
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
