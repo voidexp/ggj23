82c5b4e393bf16955c32485cd88db925d5dbbbd8
Artur Shibaylo <shibailoav@gmail.com>
2023-02-04 12:17:41 +0200

    Map blocks refactoring - renamings, array with types

--- game/level_map.gd
+++ game/level_map.gd
@@ -1,49 +1,67 @@
 extends Spatial
 
+enum BLOCK_TYPE {NONE, SOIL, ROCK, GOLD}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-export(PackedScene) var soil_brick
-export(PackedScene) var rock_brick
-export(PackedScene) var gold_brick
+export(PackedScene) var soil_block
+export(PackedScene) var rock_block
+export(PackedScene) var gold_block
 export var size_x = 21
 export var size_z = 21
 
-var GROUND_THICKNESS = 0.1
+const GROUND_THICKNESS = 0.1
+var BLOCK_TYPES_MAP = {
+	BLOCK_TYPE.SOIL: soil_block,
+	BLOCK_TYPE.ROCK: rock_block,
+	BLOCK_TYPE.GOLD: gold_block
+}
 
-var tile_map = []
+var __tile_map = []
+var __a_star = AStar.new()
 
-# Called when the node enters the scene tree for the first time.
+func clear_block(i, j):
+	pass
+
 func _ready():
+	__import_block_types()
 	__init_ground()
 	__init_tiles()
-	
+
+func __import_block_types():
+	BLOCK_TYPES_MAP = {
+	BLOCK_TYPE.SOIL: soil_block,
+	BLOCK_TYPE.ROCK: rock_block,
+	BLOCK_TYPE.GOLD: gold_block
+}
+
 func __init_tiles():
 	assert(size_x % 2 == 1)
 	var start_x = -size_x / 2
 	var start_z = -size_z / 2
 	var y_pos = GROUND_THICKNESS / 2
-	
+
 	for i in range(size_x):
+		var row = [] 
+		__tile_map.append(row)
+
 		for j in range(size_z):
 			var position = Vector3(start_x + i, y_pos, start_z + j)
-			var new_brick = __get_block_by_position(i, j).instance()
+			var block_type = __generate_block_type_by_position(i, j)
+			row.append(block_type)
+			
+			var new_block = BLOCK_TYPES_MAP[block_type].instance()
 
-			add_child(new_brick)
-			new_brick.translate(position)
+			add_child(new_block)
+			new_block.translate(position)
 
-func __get_block_by_position(i, j):
-	if i == ceil(size_x/2) and j == ceil(size_z/2):
-		return gold_brick
+func __generate_block_type_by_position(i, j):
+	if i == ceil(size_x / 2) and j == ceil(size_z / 2):
+		return BLOCK_TYPE.GOLD
 	if (i % 2 == 1) or (j % 2 == 1):
-		return soil_brick
-	return rock_brick
-	
-	
+		return BLOCK_TYPE.SOIL
+	return BLOCK_TYPE.ROCK
+
 func __init_ground():
-	var ground_size = Vector3(size_x/2, GROUND_THICKNESS/2, size_z/2)
+	var ground_size = Vector3(size_x / 2, GROUND_THICKNESS / 2, size_z / 2)
 	$Ground/CollisionShape.shape.extents = ground_size
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
