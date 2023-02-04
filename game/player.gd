41da10b2fc0fb9366f508cd74099488ef9c755d3
Ivan Nikolaev <voidexp@gmail.com>
2023-02-04 14:55:31 +0200

    Picking cooldowns + 2D info control

--- game/player.gd
+++ game/player.gd
@@ -6,14 +6,20 @@ export var color: Color
 export var player_seat: int = 1
 export var speed: int = 1
 
+export var pick_cooldown = 1.0
+export var pick_limit = 3
+
 enum {UP, DOWN, LEFT, RIGHT}
 
 var movements = []
+var picks = pick_limit
 
 func _ready():
 	name = "Player%d" % player_seat
 
 	$Model.color = color
+
+	$AnimationPlayer.connect("animation_finished", self, "__exit_cooldown")
 
 func _unhandled_input(event):
 	if event.is_action_released("p%d_move_right" % player_seat):
@@ -36,6 +42,9 @@ func _unhandled_input(event):
 
 	if event.is_action_pressed("p%d_action" % player_seat):
 		__do_pick()
+
+func _process(_delta):
+	__update_cooldown()
 
 func _physics_process(step):
 	var dir = Vector3.ZERO
@@ -64,6 +73,13 @@ func _physics_process(step):
 		pass
 
 func __do_pick():
+	if __is_in_cooldown():
+		return
+
+	picks -= 1
+	if picks == 0:
+		__enter_cooldown()
+
 	var target = $RayCast.get_collider() as Spatial
 	if not target:
 		return
@@ -72,3 +88,23 @@ func __do_pick():
 		if node.is_in_group("traits"):
 			var aspect = node as Trait
 			aspect.handle_pick(self, 10)
+
+func __enter_cooldown():
+	$AnimationPlayer.play("Cooldown", -1, 1/ pick_cooldown)
+
+func __exit_cooldown(__unused=null):
+	picks = pick_limit
+
+func __is_in_cooldown():
+	return $AnimationPlayer.is_playing()
+
+func __update_cooldown():
+	var overhead_pos = to_global(Vector3.UP)
+	$Cooldown.set_position(get_viewport().get_camera().unproject_position(overhead_pos))
+	if __is_in_cooldown():
+		$Cooldown.visible = true
+		$Cooldown.text = "%.1f" % stepify(
+			($AnimationPlayer.current_animation_length - $AnimationPlayer.current_animation_position) * pick_cooldown,
+			0.1)
+	else:
+		$Cooldown.text = "%d" % picks
