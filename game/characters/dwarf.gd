c77b6c3589ed9bd2aa487ad4619d48cf06e154f0
Ivan Nikolaev <voidexp@gmail.com>
2023-02-17 10:34:11 +0200

    Make dwarf's helmet material local to instance
    Slight optimization, avoids creating new material for the dwarf's helmet
    on each color change.

--- game/characters/dwarf.gd
+++ game/characters/dwarf.gd
@@ -4,6 +4,4 @@ export var color: Color = Color.azure setget _set_color
 
 func _set_color(new_color):
 	color = new_color
-	var helmet_mat = SpatialMaterial.new()
-	helmet_mat.albedo_color = color
-	$RootNode/group1/helmet.set_surface_material(0, helmet_mat)
+	$RootNode/group1/helmet.get_surface_material(0).albedo_color = color
